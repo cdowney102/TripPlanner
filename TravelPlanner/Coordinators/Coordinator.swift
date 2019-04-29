f44e7a389f8b00b0d78651557de4d67091295d37
Chris <cdowney102@gmail.com>
2019-04-28 19:33:34 -0700

    first commit. Removed storyboard. Setup main Coordinator

---
+++ TravelPlanner/Coordinators/Coordinator.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator.swift
+//  TravelPlanner
+//
+//  Created by christopher downey on 4/28/19.
+//  Copyright © 2019 christopher downey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Coordinator {
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: UINavigationController { get set }
+    
+    func start()
+}
---
+++ TravelPlanner/Coordinators/MainCoordinator.swift
@@ -0,0 +1,24 @@
+//
+//  MainCoordinator.swift
+//  TravelPlanner
+//
+//  Created by christopher downey on 4/28/19.
+//  Copyright © 2019 christopher downey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = HomeController()
+        navigationController.pushViewController(vc, animated: true)
+    }
+}
